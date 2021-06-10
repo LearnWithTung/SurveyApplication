a87b25f0fd7bfbf9779d25fcbe388bc8c5575a0a
TungVu <tungvuduc2805@gmail.com>
2021-06-10 20:16:24 +0700

    `login` signs body request with parameters

--- SurveyFramework/SurveyFrameworkTests/LoginUseCaseTests.swift
+++ SurveyFramework/SurveyFrameworkTests/LoginUseCaseTests.swift
@@ -12,18 +12,36 @@ struct LoginInfo {
     let password: String
 }
 
+struct Credentials {
+    let client_id: String
+    let client_secret: String
+}
+
 class RemoteLoginService {
     private let url: URL
     private let client: HTTPClient
-
-    init(url: URL, client: HTTPClient) {
+    private let credentials: Credentials
+    
+    init(url: URL, client: HTTPClient, credentials: Credentials) {
         self.client = client
         self.url = url
+        self.credentials = credentials
     }
     
     func login(with info: LoginInfo) {
+        let body = [
+            "grant_type": "password",
+            "email": info.email,
+            "password": info.password,
+            "client_id": credentials.client_id,
+            "client_secret": credentials.client_secret
+        ]
+        let bodyData = try! JSONSerialization.data(withJSONObject: body)
+        
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
+        request.httpBody = bodyData
+        
         client.post(with: request)
     }
 }
@@ -53,10 +71,31 @@ class LoginUseCaseTests: XCTestCase {
         XCTAssertEqual(client.requestedURL?.url, url)
     }
     
+    func test_login_signsRequestWithBodyParams() {
+        let url = URL(string: "https://a-url.com")!
+        let credentials = Credentials(client_id: "a clientId", client_secret: "a secret")
+        let info = LoginInfo(email: "an email", password: "a password")
+        let body = [
+            "grant_type": "password",
+            "email": info.email,
+            "password": info.password,
+            "client_id": credentials.client_id,
+            "client_secret": credentials.client_secret
+        ]
+        let (sut, client) = makeSUT(url: url, credentials: credentials)
+        
+        sut.login(with: info)
+        
+        let requestedBody = try! JSONSerialization.jsonObject(with: client.requestedURL!.httpBody!) as! [String: String] 
+        
+        XCTAssertEqual(client.requestedURL?.httpMethod, "POST")
+        XCTAssertEqual(requestedBody, body)
+    }
+    
     // MARK: - Helpers
-    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteLoginService, client: HTTPClient) {
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!, credentials: Credentials = Credentials(client_id: "any", client_secret: "any")) -> (sut: RemoteLoginService, client: HTTPClient) {
         let client = HTTPClient()
-        let sut = RemoteLoginService(url: url, client: client)
+        let sut = RemoteLoginService(url: url, client: client, credentials: credentials)
         
         return (sut, client)
     }
