0700864cf4fecb730badbbe4362be639b9112b7c
TungVu <tungvuduc2805@gmail.com>
2021-06-10 22:12:02 +0700

    `KeychainTokenStore.load` returns error on empty cache

---
+++ SurveyFramework/SurveyFrameworkTests/KeychainTokenStoreTests.swift
@@ -0,0 +1,39 @@
+//
+//  KeychainTokenStoreTests.swift
+//  SurveyFrameworkTests
+//
+//  Created by Tung Vu on 10/06/2021.
+//
+
+import XCTest
+
+class KeychainTokenStore {
+
+    enum Error: Swift.Error {
+        case dataNotFound
+    }
+
+    func load(completion: @escaping (Result<Any, Swift.Error>) -> Void) {
+        completion(.failure(Error.dataNotFound))
+    }
+
+}
+
+class KeychainTokenStoreTests: XCTestCase {
+
+    func test_load_returnsErrorWhenNothingSaved() {
+        let sut = KeychainTokenStore()
+        let exp = expectation(description: "Wait for completion")
+
+        var capturedResult: Result<Any, Error>?
+        sut.load {
+            capturedResult = $0.mapError { $0 as NSError }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 0.1)
+
+        XCTAssertThrowsError(try capturedResult?.get())
+    }
+
+}
