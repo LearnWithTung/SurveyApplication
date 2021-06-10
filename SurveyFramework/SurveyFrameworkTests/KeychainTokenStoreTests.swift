5f8e77d296b10e8b55351cdf7d1a4a11f9da8da7
TungVu <tungvuduc2805@gmail.com>
2021-06-10 22:21:06 +0700

    `load` returns saved token

--- SurveyFramework/SurveyFrameworkTests/KeychainTokenStoreTests.swift
+++ SurveyFramework/SurveyFrameworkTests/KeychainTokenStoreTests.swift
@@ -6,15 +6,30 @@
 //
 
 import XCTest
+import SurveyFramework
 
 class KeychainTokenStore {
+    struct LocalToken: Codable {
+        let accessToken: String
+        let tokenType: String
+        let expiredDate: Date
+        let refreshToken: String
+        
+        init(tokens: Token) {
+            self.accessToken = tokens.accessToken
+            self.tokenType = tokens.tokenType
+            self.expiredDate = tokens.expiredDate
+            self.refreshToken = tokens.refreshToken
+        }
+        
+        var model: Token {
+            return Token(accessToken: accessToken, tokenType: tokenType, expiredDate: expiredDate, refreshToken: refreshToken)
+        }
+    }
 
     enum Error: Swift.Error {
         case dataNotFound
-    }
-
-    func load(completion: @escaping (Result<Any, Swift.Error>) -> Void) {
-        completion(.failure(Error.dataNotFound))
+        case saveFailed
     }
     
     private var key: String {
@@ -29,6 +44,49 @@ class KeychainTokenStore {
         
         SecItemDelete(query)
     }
+    
+    func save(tokens: Token, completion: @escaping (Result<Void, Swift.Error>) -> Void) {
+        do {
+            let data = try JSONEncoder().encode(LocalToken(tokens: tokens))
+            
+            let query = [
+                kSecClass: kSecClassGenericPassword,
+                kSecAttrAccount: key,
+                kSecValueData: data
+            ] as CFDictionary
+            
+            guard SecItemAdd(query, nil) == noErr else {
+                throw Error.saveFailed
+            }
+            
+            completion(.success(()))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func load(completion: @escaping (Result<Token, Swift.Error>) -> Void) {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanTrue as Any,
+            kSecMatchLimit: kSecMatchLimit
+        ] as CFDictionary
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query, &result)
+        
+        guard status == noErr, let data = result as? Data else {
+            return completion(.failure(Error.dataNotFound))
+        }
+        
+        do {
+            let token = try JSONDecoder().decode(LocalToken.self, from: data)
+            completion(.success(token.model))
+        } catch {
+            completion(.failure(error))
+        }
+    }
 }
 
 class KeychainTokenStoreTests: XCTestCase {
@@ -37,7 +95,7 @@ class KeychainTokenStoreTests: XCTestCase {
         let sut = makeSUT()
         let exp = expectation(description: "Wait for completion")
 
-        var capturedResult: Result<Any, Error>?
+        var capturedResult: Result<Token, Error>?
         sut.load {
             capturedResult = $0.mapError { $0 as NSError }
             exp.fulfill()
@@ -48,12 +106,42 @@ class KeychainTokenStoreTests: XCTestCase {
         XCTAssertThrowsError(try capturedResult?.get())
     }
     
+    func test_load_returnsSavedToken() {
+        let sut = KeychainTokenStore()
+        let saveExp = expectation(description: "Wait for saving completion")
+        let loadExp = expectation(description: "Wait for loading completion")
+        let token = makeToken()
+        
+        sut.save(tokens: token) { _ in
+            saveExp.fulfill()
+        }
+        
+        wait(for: [saveExp], timeout: 0.1)
+        
+        var capturedResult: Result<Token, Error>?
+        sut.load {
+            capturedResult = $0
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 0.1)
+        
+        XCTAssertEqual(try? capturedResult?.get(), token)
+    }
+    
     // MARK: - Helpers
     func makeSUT() -> KeychainTokenStore {
         let sut = KeychainTokenStore()
         KeychainTokenStore().clear()
         checkForMemoryLeaks(sut)
         return sut
+    }
+    
+    private func makeToken(accessToken: String = "any", tokenType: String = "any", expiredDate: Date = Date(), refreshToken: String = "any") -> Token {
+        Token(accessToken: accessToken,
+              tokenType: tokenType,
+              expiredDate: expiredDate,
+              refreshToken: refreshToken)
     }
 
 }
