904cbb06bd03842809c50dad03cdb3676d1e3080
TungVu <tungvuduc2805@gmail.com>
2021-06-10 19:57:25 +0700

    `login` requests data from URL

--- SurveyFramework/SurveyFrameworkTests/LoginUseCaseTests.swift
+++ SurveyFramework/SurveyFrameworkTests/LoginUseCaseTests.swift
@@ -15,11 +15,18 @@ class RemoteLoginService {
         self.client = client
         self.url = url
     }
+    
+    func login() {
+        client.post(with: url)
+    }
 }
 
 class HTTPClient {
     var requestedURL: URL?
 
+    func post(with url: URL) {
+        requestedURL = url
+    }
 }
 
 class LoginUseCaseTests: XCTestCase {
@@ -30,6 +37,16 @@ class LoginUseCaseTests: XCTestCase {
         _ = RemoteLoginService(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_login_requestsDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
+        let client = HTTPClient()
+        let sut = RemoteLoginService(url: url, client: client)
+        
+        sut.login()
+        
+        XCTAssertEqual(client.requestedURL, url)
     }
 
 }
