aad3db619491a37ee47168bad8a83b0bea6425d5
TungVu <tungvuduc2805@gmail.com>
2021-06-10 22:35:14 +0700

    mark `KeychainTokenStore` as final class

--- SurveyFramework/SurveyFramework/Local/KeychainTokenStore.swift
+++ SurveyFramework/SurveyFramework/Local/KeychainTokenStore.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class KeychainTokenStore {
+public final class KeychainTokenStore {
     struct LocalToken: Codable {
         let accessToken: String
         let tokenType: String
