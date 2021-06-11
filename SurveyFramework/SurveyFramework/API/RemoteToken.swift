8d4ad0700d0309010c3b3e0d668c116fa81bbb7e
TungVu <tungvuduc2805@gmail.com>
2021-06-11 08:51:44 +0700

    move RemoteToken model to a separated file

---
+++ SurveyFramework/SurveyFramework/API/RemoteToken.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteToken.swift
+//  SurveyFramework
+//
+//  Created by Tung Vu on 11/06/2021.
+//
+
+import Foundation
+
+struct Root: Decodable {
+    let attributes: RemoteToken
+    
+    struct RemoteToken: Decodable {
+        let access_token: String
+        let token_type: String
+        let expires_in: Int
+        let refresh_token: String
+        
+        func toModel(currentDate: Date) -> Token {
+            let calendar = Calendar(identifier: .gregorian)
+            let expiredDate = calendar.date(byAdding: .second, value: expires_in, to: currentDate)!
+            let token = Token(accessToken: access_token, tokenType: token_type, expiredDate: expiredDate, refreshToken: refresh_token)
+            
+            return token
+        }
+    }
+}
--- SurveyFramework/SurveyFramework/API/RemoteTokenMappers.swift
+++ SurveyFramework/SurveyFramework/API/RemoteTokenMappers.swift
@@ -7,25 +7,6 @@
 
 import Foundation
 
-struct Root: Decodable {
-    let attributes: RemoteToken
-    
-    struct RemoteToken: Decodable {
-        let access_token: String
-        let token_type: String
-        let expires_in: Int
-        let refresh_token: String
-        
-        func toModel(currentDate: Date) -> Token {
-            let calendar = Calendar(identifier: .gregorian)
-            let expiredDate = calendar.date(byAdding: .second, value: expires_in, to: currentDate)!
-            let token = Token(accessToken: access_token, tokenType: token_type, expiredDate: expiredDate, refreshToken: refresh_token)
-            
-            return token
-        }
-    }
-}
-
 final class RemoteTokenMappers {
 
     private init() {}
