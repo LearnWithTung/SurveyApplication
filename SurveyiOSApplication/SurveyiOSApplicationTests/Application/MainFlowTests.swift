ac5478340886420dd00b27c752b1085cf74004e3
TungVu <tungvuduc2805@gmail.com>
2021-06-13 15:31:58 +0700

    `start` set view controller

--- SurveyiOSApplication/SurveyiOSApplicationTests/Application/MainFlowTests.swift
+++ SurveyiOSApplication/SurveyiOSApplicationTests/Application/MainFlowTests.swift
@@ -10,9 +10,22 @@ import UIKit
 import SurveyiOSApplication
 
 class MainFlow {
+    private let navController: UINavigationController
+    private let delegate: HomeViewControllerDelegate
+    private let currentDate: () -> Date
     
-    public init(navController: UINavigationController) {
+    public init(navController: UINavigationController,
+                delegate: HomeViewControllerDelegate,
+                currentDate: @escaping () -> Date) {
+        self.navController = navController
+        self.delegate = delegate
+        self.currentDate = currentDate
+    }
+    
+    func start(){
+        let vc = HomeUIComposer.homeComposedWith(delegate: delegate, currentDate: currentDate)
         
+        navController.setViewControllers([vc], animated: true)
     }
     
 }
@@ -25,15 +38,42 @@ class MainFlowTests: XCTestCase {
         XCTAssertEqual(nc.messages, [])
     }
     
+    func test_start_setsViewController() {
+        let (sut, nc) = makeSUT()
+
+        sut.start()
+        
+        XCTAssertEqual(nc.messages.count, 1)
+        XCTAssertEqual(nc.messages[0].viewControllers?.count, 1)
+        XCTAssertEqual(nc.messages[0].animated, true)
+
+        let homeViewController = getHomeViewController(from: nc)
+
+        XCTAssertNotNil(homeViewController)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: MainFlow, navigationController: NavigationControllerSpy) {
         let navigationControllerSpy = NavigationControllerSpy()
-        let sut = MainFlow(navController: navigationControllerSpy)
+        let delegateSpy = DelegateSpy()
+        let sut = MainFlow(navController: navigationControllerSpy, delegate: delegateSpy, currentDate: Date.init)
         
+        checkForMemoryLeaks(delegateSpy, file: file, line: line)
         checkForMemoryLeaks(navigationControllerSpy, file: file, line: line)
         checkForMemoryLeaks(sut, file: file, line: line)
 
         return (sut, navigationControllerSpy)
+    }
+    
+    private class DelegateSpy: HomeViewControllerDelegate {
+        func loadSurvey(pageNumber: Int, pageSize: Int, completion: @escaping (Result<[RepresentationSurvey], Error>) -> Void) {}
+    }
+    
+    private func getHomeViewController(from navigationController: NavigationControllerSpy) -> HomeViewController? {
+        let rootVc = navigationController.messages[0].viewControllers?.first
+        rootVc?.loadViewIfNeeded()
+        
+        return rootVc as? HomeViewController
     }
     
 }
