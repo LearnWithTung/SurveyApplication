b99b1633bb31b45358e7b5347ce3ba2e4701275c
TungVu <tungvuduc2805@gmail.com>
2021-06-14 09:28:16 +0700

    add SideMenuViewController class

---
+++ SurveyiOSApplication/SurveyiOSApplication/Controllers/SideMenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SideMenuViewController.swift
+//  SurveyiOSApplication
+//
+//  Created by Tung Vu on 14/06/2021.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    
+}
